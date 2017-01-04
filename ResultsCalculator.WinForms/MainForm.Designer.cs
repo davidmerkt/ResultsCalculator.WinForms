0d5a01938bfcc26728874ee8bba03ba055227ac5
David Merkt <davidmerkt@users.noreply.github.com>
2017-01-03 23:32:37 -0500

    Added basic form controls

--- ResultsCalculator.WinForms/MainForm.Designer.cs
+++ ResultsCalculator.WinForms/MainForm.Designer.cs
@@ -29,11 +29,158 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.runnerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startTime = new System.Windows.Forms.DateTimePicker();
+            this.finishTime = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lapsCount = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.resultsBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lapsCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Runner Name";
+            // 
+            // runnerName
+            // 
+            this.runnerName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runnerName.Location = new System.Drawing.Point(91, 12);
+            this.runnerName.Name = "runnerName";
+            this.runnerName.Size = new System.Drawing.Size(181, 20);
+            this.runnerName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Start Time";
+            // 
+            // startTime
+            // 
+            this.startTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.startTime.Location = new System.Drawing.Point(91, 38);
+            this.startTime.Name = "startTime";
+            this.startTime.Size = new System.Drawing.Size(181, 20);
+            this.startTime.TabIndex = 3;
+            // 
+            // finishTime
+            // 
+            this.finishTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.finishTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.finishTime.Location = new System.Drawing.Point(91, 64);
+            this.finishTime.Name = "finishTime";
+            this.finishTime.Size = new System.Drawing.Size(181, 20);
+            this.finishTime.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Finish Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Number of Laps";
+            // 
+            // lapsCount
+            // 
+            this.lapsCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lapsCount.Location = new System.Drawing.Point(91, 90);
+            this.lapsCount.Name = "lapsCount";
+            this.lapsCount.Size = new System.Drawing.Size(181, 20);
+            this.lapsCount.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsBox.Location = new System.Drawing.Point(12, 145);
+            this.resultsBox.Multiline = true;
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.ReadOnly = true;
+            this.resultsBox.Size = new System.Drawing.Size(260, 104);
+            this.resultsBox.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lapsCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.finishTime);
+            this.Controls.Add(this.startTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.runnerName);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Results Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.lapsCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox runnerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker startTime;
+        private System.Windows.Forms.DateTimePicker finishTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown lapsCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox resultsBox;
     }
 }
 
--- ResultsCalculator.WinForms/MainForm.cs
+++ ResultsCalculator.WinForms/MainForm.cs
@@ -12,9 +12,19 @@ namespace ResultsCalculator.WinForms
 {
     public partial class MainForm : Form
     {
+        Runner newRunner;
+
         public MainForm()
         {
             InitializeComponent();
+            resultsBox.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newRunner = new Runner() { Name = runnerName.Text, StartTime = startTime.Value, EndTime = finishTime.Value, TotalLaps = Convert.ToInt16(lapsCount.Value) };
+            resultsBox.Text = newRunner.Name + Environment.NewLine + newRunner.TotalLaps + Environment.NewLine + newRunner.TotalTime.ToString();
+            resultsBox.Visible = true;
         }
     }
 }
